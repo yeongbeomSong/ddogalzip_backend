ca49e7b9973790c038b3f1a9779d2d771ed0da26
yeongbeomSong <s9091002@gmail.com>
2024-02-04 22:02:20 +0900

    first commit

---
+++ src/main/java/com/ddogalzip/ddogalzip/DdogalzipApplication.java
@@ -0,0 +1,14 @@
+package com.ddogalzip.ddogalzip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdogalzipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdogalzipApplication.class, args);
+		System.out.print("hello");
+	}
+
+}
---
+++ src/test/java/com/ddogalzip/ddogalzip/DdogalzipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddogalzip.ddogalzip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdogalzipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
